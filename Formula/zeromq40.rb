779dd62b83094ad78eee661f704052ca703120ac
Gaston Jorquera <gjorquera@gmail.com>
2016-06-16 17:40:24 -0400

    Update zeromq formula

--- Formula/zeromq40.rb
+++ Formula/zeromq40.rb
@@ -4,37 +4,15 @@ class Zeromq40 < Formula
   url "http://download.zeromq.org/zeromq-4.0.4.tar.gz"
   sha256 "1ef71d46e94f33e27dd5a1661ed626cd39be4d2d6967792a275040e34457d399"
 
-  option :universal
-  option "with-libpgm", "Build with PGM extension"
-  option "with-norm", "Build with NORM extension"
-
-  deprecated_option "with-pgm" => "with-libpgm"
-
   depends_on "pkg-config" => :build
-  depends_on "libpgm" => :optional
-  depends_on "libsodium" => :optional
-  depends_on "norm" => :optional
 
   def install
-    ENV.universal_binary if build.universal?
+    # Force compilation via gcc, clang wasn't exporting object symnbols.
+    ENV['HOMEBREW_CC'] = ENV['CC'] = '/usr/bin/gcc'
+    ENV['HOMEBREW_CXX'] = ENV['CXX'] = '/usr/bin/g++'
 
-    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
-    if build.with? "libpgm"
-      # Use HB libpgm-5.2 because their internal 5.1 is b0rked.
-      ENV["pgm_CFLAGS"] = `pkg-config --cflags openpgm-5.2`.chomp
-      ENV["pgm_LIBS"] = `pkg-config --libs openpgm-5.2`.chomp
-      args << "--with-pgm"
-    end
+    args = ["--prefix=#{prefix}"]
 
-    if build.with? "libsodium"
-      args << "--with-libsodium"
-    else
-      args << "--without-libsodium"
-    end
-
-    args << "--with-norm" if build.with? "norm"
-
-    system "./autogen.sh" if build.head?
     system "./configure", *args
     system "make"
     system "make", "install"
